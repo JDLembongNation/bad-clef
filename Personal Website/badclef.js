db7520b32545339865d3236a532bc4c67d402ffe
Joshua Domenico Lembong <lembongjoshua@gmail.com>
2018-07-02 09:37:50 +0700

    Update Inclusive of Blog
    Take that boi

--- Personal Website/badclef.js
+++ Personal Website/badclef.js
@@ -1,6 +1,4 @@
 $(document).ready(() => {
-    $('.test').typeTo("B A D C L E F - Unreadable and Unlikeable but Powerful...");
-    $('.content').typeTo("Significance of life and harmony, Rebalanced and Reimagined");
-    $('.coming').typeTo("Coming Soon");
-    $('.subHeader').typeTo("9/12/18");
+    $('.test').typeTo("B A D C L E F - Your gateway to graceful creativity");
+    $('.content').typeTo("Creativity supplied with modern day thinking");
 });
